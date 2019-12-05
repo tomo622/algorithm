e883adaa62714df73b994fec759d40674c97b1c5
tomo <shindohyun00@gmail.com>
2019-12-06 01:23:56 +0900

    문자열 패턴 매칭 수정
    브루트포스를 이용한 문자열 패턴 매칭 수정
    - 텍스트 배열 범위 체크
    - 일치 개수 반환

--- string_pattern_matching/brute_force/main.java
+++ string_pattern_matching/brute_force/main.java
@@ -2,16 +2,20 @@
 public class main {
 
 	public static void main(String[] args) {
-		String text = "TTTTATTATTTCTAACCAA";
-		String pattern = "TTATTTCTA";
+		String text = "ABABA";//"TTTTATTATTTCTAACCAA";
+		String pattern = "ABA";//"TTATTTCTA";
 		
-		System.out.println(bruteForce(text, pattern));
+		System.out.println(bruteForce(text, pattern) + "개 찾았습니다.");
 	}
 	
-	private static boolean bruteForce(String text, String pattern) {
-		boolean found = false;
+	private static int bruteForce(String text, String pattern) {
+		int foundCnt = 0;
 		
 		for(int i = 0; i < text.length(); i++) {
+			if(i + pattern.length() > text.length()) {
+				break;
+			}
+			
 			int j = 0;
 			for(;j < pattern.length(); j++) {
 				if(text.charAt(i+j) != pattern.charAt(j)) {
@@ -20,11 +24,10 @@ public class main {
 			}
 			
 			if(j == pattern.length()) {
-				found = true;
-				break;
+				foundCnt++;
 			}
 		}
 		
-		return found;
+		return foundCnt;
 	}
 }
